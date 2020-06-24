498ccbaa0c8bf65431075de94f904bbb41d88a73
Jeremiah <jeremiah.w.hahn@gmail.com>
2020-06-24 19:30:15 -0400

    Add project files.

---
+++ README.md
@@ -0,0 +1,8 @@
+# StopFoodDecay
+A mod for Outward that prevents all perishable food items from decaying, meaning you won't have to worry about your food rotting.  The mod has been rewritten for the release of The Soroboreans DLC, but it will work without the DLC as well.
+
+## Requirements
+Requires [BepInEx](https://github.com/BepInEx/BepInEx/releases "BepInEx Release Versions") version 5.1 or later.
+
+## Installation
+Instructions for installing mods can be found [here](https://outward.gamepedia.com/Installing_Mods "Installing Mods - Official Outward Wiki").  If you use Steam or GoG then the most important step is to switch to the [modding branch](https://outward.gamepedia.com/Installing_Mods#Modding_Branch "Modding Branch") of Outward.
---
+++ StopFoodDecay/StopFoodDecay.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace StopFoodDecay
+{
+    [BepInPlugin(ID, NAME, VERSION)]
+    public class MyMod : BaseUnityPlugin
+    {
+        const string ID = "com.Jeremiah.StopFoodDecay";
+        const string NAME = "Stop Food Decay";
+        const string VERSION = "2.1";
+
+        internal void Awake()
+        {
+            var harmony = new Harmony(ID);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Item), "OnAwake")]
+    class Item_OnAwake_Patch
+    {
+        //private static readonly BepInEx.Logging.ManualLogSource PatchLogger = BepInEx.Logging.Logger.CreateLogSource("StopFoodDecay.Item_OnAwake_Patch");
+
+        [HarmonyPostfix]
+        public static void Postfix(ref Item __instance)
+        {
+            if (__instance && (__instance.IsFood || (string.Equals(__instance.TypeDisplay, "Food") && __instance.IsPerishable)))
+            {
+                Item.Destroy(__instance.GetComponent("Perishable"));
+            }
+        }
+    }
+}
